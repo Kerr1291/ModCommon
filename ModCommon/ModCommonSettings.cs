1e7825a37a9cae7d429cebf41aacdfadd00686ee
Eli Stone <eli.stonium@gmail.com>
2018-07-05 20:12:25 -0700

    Add version int to make external mod version checks easier.

--- ModCommon/ModCommonSettings.cs
+++ ModCommon/ModCommonSettings.cs
@@ -8,9 +8,13 @@ namespace ModCommon
 {
     public class ModCommonSettingsVars
     {
-        //change when the global settings are updated to force a recreation of the global settings
-        public const string ModCommonVersion = "0.0.4";
-        //change when the global settings are updated to force a recreation of the global settings
+        // change when ModCommon is updated so mods that rely on it can ensure they have the proper functionality to run.
+        // This variable exists to be read and used implicitly by external mods. Either through reflection, or directly.
+        public const int ABIVersion = 5;
+        
+        // change when you update the abi version so ModLog.txt dumps / screenshots are more useful.
+        public const string ModCommonVersion = "0.0.5";
+        // change when the global settings are updated to force a recreation of the global settings
         public const string GlobalSettingsVersion = "0.0.2";
     }
 
