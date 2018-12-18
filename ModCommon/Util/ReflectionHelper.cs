a47ab5c102f10621130ff0a55e67852d3dac9a97
5FiftySix6 <ybham6@gmail.com>
2018-12-17 20:55:55 -0500

    Remove parts of ReflectionHelper as they've been moved to the API.

--- ModCommon/Util/ReflectionHelper.cs
+++ ModCommon/Util/ReflectionHelper.cs
@@ -1,64 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
-using System.Runtime.Remoting.Messaging;
 using MonoMod.Utils;
 
 namespace ModCommon.Util
 {
     public static class ReflectionHelper
     {
-        private static readonly Dictionary<Type, Dictionary<string, FieldInfo>> Fields =
-            new Dictionary<Type, Dictionary<string, FieldInfo>>();
-
-        public static T GetAttr<T>(object obj, string name, bool instance = true)
-        {
-            if (obj == null || string.IsNullOrEmpty(name)) return default(T);
-
-            Type t = obj.GetType();
-
-            if (!Fields.ContainsKey(t))
-            {
-                Fields.Add(t, new Dictionary<string, FieldInfo>());
-            }
-
-            Dictionary<string, FieldInfo> typeFields = Fields[t];
-
-            if (!typeFields.ContainsKey(name))
-            {
-                typeFields.Add(name,
-                    t.GetField(name,
-                        BindingFlags.NonPublic | BindingFlags.Public |
-                        (instance ? BindingFlags.Instance : BindingFlags.Static)));
-            }
-
-            return (T) typeFields[name]?.GetValue(obj);
-        }
-
-        public static void SetAttr<T>(object obj, string name, T val, bool instance = true)
-        {
-            if (obj == null || string.IsNullOrEmpty(name)) return;
-
-            Type t = obj.GetType();
-
-            if (!Fields.ContainsKey(t))
-            {
-                Fields.Add(t, new Dictionary<string, FieldInfo>());
-            }
-
-            Dictionary<string, FieldInfo> typeFields = Fields[t];
-
-            if (!typeFields.ContainsKey(name))
-            {
-                typeFields.Add(name,
-                    t.GetField(name,
-                        BindingFlags.NonPublic | BindingFlags.Public |
-                        (instance ? BindingFlags.Instance : BindingFlags.Static)));
-            }
-
-            typeFields[name]?.SetValue(obj, val);
-        }
-        
         private static readonly Dictionary<Type, Dictionary<string, MethodInfo>> METHOD_INFOS =
             new Dictionary<Type, Dictionary<string, MethodInfo>>();
 
@@ -92,13 +40,14 @@ namespace ModCommon.Util
         }
     }
 
+    
     public static class ReflectionExtensions
     {
         public static void SetAttr<T>(this object obj, string name, T val, bool instance = true) =>
-            ReflectionHelper.SetAttr(obj, name, val, instance);
+            Modding.ReflectionHelper.SetAttr(obj, name, val, instance);
 
         public static T GetAttr<T>(this object obj, string name, bool instance = true) =>
-            ReflectionHelper.GetAttr<T>(obj, name, instance);
+            Modding.ReflectionHelper.GetAttr<T>(obj, name, instance);
 
         public static MethodInfo GetMethodInfo(this object obj, string name, bool instance = true) =>
             ReflectionHelper.GetMethodInfo(obj, name, instance);
